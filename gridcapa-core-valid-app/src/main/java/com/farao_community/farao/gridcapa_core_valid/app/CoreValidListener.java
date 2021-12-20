96276b0374893b791b881015e16a33c1bc70ad44
Montigny <alexandre.montigny@rte-france.com>
2021-12-20 17:19:43 +0100

    Change status of a task in CoreValidListener (WIP)

--- gridcapa-core-valid-app/src/main/java/com/farao_community/farao/gridcapa_core_valid/app/CoreValidListener.java
+++ gridcapa-core-valid-app/src/main/java/com/farao_community/farao/gridcapa_core_valid/app/CoreValidListener.java
@@ -12,11 +12,18 @@ import com.farao_community.farao.core_valid.api.exception.AbstractCoreValidExcep
 import com.farao_community.farao.core_valid.api.exception.CoreValidInternalException;
 import com.farao_community.farao.core_valid.api.resource.CoreValidRequest;
 import com.farao_community.farao.core_valid.api.resource.CoreValidResponse;
+import com.farao_community.farao.gridcapa.task_manager.api.TaskStatus;
+import com.farao_community.farao.gridcapa.task_manager.api.TaskStatusUpdate;
 import com.farao_community.farao.gridcapa_core_valid.app.configuration.AmqpMessagesConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.*;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+import java.util.function.Function;
 
 /**
  * @author Ameni Walha {@literal <ameni.walha at rte-france.com>}
@@ -29,13 +36,16 @@ public class CoreValidListener implements MessageListener {
     private static final String CONTENT_ENCODING = "UTF-8";
     private static final String CONTENT_TYPE = "application/vnd.api+json";
     private static final int PRIORITY = 1;
+    private static final String TASK_STATUS_UPDATE = "task-status-update";
 
     private final JsonApiConverter jsonApiConverter;
     private final AmqpTemplate amqpTemplate;
     private final CoreValidHandler coreValidHandler;
     private final AmqpMessagesConfiguration amqpMessagesConfiguration;
+    private final StreamBridge streamBridge;
 
-    public CoreValidListener(CoreValidHandler coreValidHandler, AmqpTemplate amqpTemplate, AmqpMessagesConfiguration amqpMessagesConfiguration) {
+    public CoreValidListener(CoreValidHandler coreValidHandler, AmqpTemplate amqpTemplate, AmqpMessagesConfiguration amqpMessagesConfiguration, StreamBridge streamBridge) {
+        this.streamBridge = streamBridge;
         this.jsonApiConverter = new JsonApiConverter();
         this.coreValidHandler = coreValidHandler;
         this.amqpTemplate = amqpTemplate;
@@ -46,23 +56,25 @@ public class CoreValidListener implements MessageListener {
     public void onMessage(Message message) {
         String replyTo = message.getMessageProperties().getReplyTo();
         String correlationId = message.getMessageProperties().getCorrelationId();
+        CoreValidRequest coreValidRequest = jsonApiConverter.fromJsonMessage(message.getBody(), CoreValidRequest.class);
         try {
-            CoreValidRequest coreValidRequest = jsonApiConverter.fromJsonMessage(message.getBody(), CoreValidRequest.class);
+            streamBridge.send(TASK_STATUS_UPDATE, new TaskStatusUpdate(UUID.fromString(coreValidRequest.getId()), TaskStatus.RUNNING));
             LOGGER.info("Core valid request received: {}", coreValidRequest);
             CoreValidResponse coreValidResponse = coreValidHandler.handleCoreValidRequest(coreValidRequest);
             LOGGER.info("Core valid response sent: {}", coreValidResponse);
             sendCoreValidResponse(coreValidResponse, replyTo, correlationId);
         } catch (AbstractCoreValidException e) {
             LOGGER.error("Core valid exception occured", e);
-            sendErrorResponse(e, replyTo, correlationId);
+            sendErrorResponse(coreValidRequest.getId(), e, replyTo, correlationId);
         } catch (Exception e) {
             LOGGER.error("Unknown exception occured", e);
             AbstractCoreValidException wrappingException = new CoreValidInternalException("Unknown exception", e);
-            sendErrorResponse(wrappingException, replyTo, correlationId);
+            sendErrorResponse(coreValidRequest.getId(), wrappingException, replyTo, correlationId);
         }
     }
 
-    private void sendErrorResponse(AbstractCoreValidException e, String replyTo, String correlationId) {
+    private void sendErrorResponse(String requestId, AbstractCoreValidException e, String replyTo, String correlationId) {
+        streamBridge.send(TASK_STATUS_UPDATE, new TaskStatusUpdate(UUID.fromString(requestId), TaskStatus.ERROR));
         if (replyTo != null) {
             amqpTemplate.send(replyTo, createErrorResponse(e, correlationId));
         } else {
@@ -71,6 +83,7 @@ public class CoreValidListener implements MessageListener {
     }
 
     private void sendCoreValidResponse(CoreValidResponse coreValidResponse, String replyTo, String correlationId) {
+        streamBridge.send(TASK_STATUS_UPDATE, new TaskStatusUpdate(UUID.fromString(coreValidResponse.getId()), TaskStatus.SUCCESS));
         if (replyTo != null) {
             amqpTemplate.send(replyTo, createMessageResponse(coreValidResponse, correlationId));
         } else {
