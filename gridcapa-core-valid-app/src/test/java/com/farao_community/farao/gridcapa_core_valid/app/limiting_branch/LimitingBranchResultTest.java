33ce144cd53eaa5d3c560ec873051ce1ebf72cf1
Montigny <alexandre.montigny@rte-france.com>
2022-02-23 14:13:45 +0100

    Increase code coverage

--- gridcapa-core-valid-app/src/test/java/com/farao_community/farao/gridcapa_core_valid/app/limiting_branch/LimitingBranchResultTest.java
+++ gridcapa-core-valid-app/src/test/java/com/farao_community/farao/gridcapa_core_valid/app/limiting_branch/LimitingBranchResultTest.java
@@ -7,19 +7,27 @@
 
 package com.farao_community.farao.gridcapa_core_valid.app.limiting_branch;
 
+import com.farao_community.farao.core_valid.api.exception.CoreValidInvalidDataException;
+import com.farao_community.farao.data.crac_api.Instant;
+import com.farao_community.farao.data.crac_api.State;
 import com.farao_community.farao.data.crac_creation.creator.fb_constraint.crac_creator.FbConstraintCreationContext;
 import com.farao_community.farao.gridcapa_core_valid.app.services.FileImporter;
 import com.farao_community.farao.gridcapa_core_valid.app.study_point.StudyPoint;
 import com.powsybl.iidm.import_.Importers;
 import com.powsybl.iidm.network.Network;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.OffsetDateTime;
+import java.util.HashSet;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * @author Theo Pascoli {@literal <theo.pascoli at rte-france.com>}
@@ -29,12 +37,29 @@ class LimitingBranchResultTest {
 
     private final String raoDirectory = "/rao-result";
     private final OffsetDateTime dateTime = OffsetDateTime.parse("2021-07-22T22:30Z");
+    private State state;
+    private LimitingBranchResult limitingBranchResult;
 
     @Autowired
     private LimitingBranchResultService limitingBranchResultService;
 
     @Autowired
     private FileImporter fileImporter;
+
+    @BeforeEach
+    void setUp() {
+        state = Mockito.mock(State.class);
+        limitingBranchResult = new LimitingBranchResult(
+                "verticeId",
+                "criticalBranchId",
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                new HashSet<>(),
+                "criticalBranchName",
+                state);
+    }
 
     @Test
     void importRaoResultTest() {
@@ -53,4 +78,36 @@ class LimitingBranchResultTest {
         assertEquals(1939, Math.floor(limitingBranchResults.get(4).getFlowBefore()));
     }
 
+    @Test
+    void getCriticalBranchName() {
+        assertEquals("criticalBranchName", limitingBranchResult.getCriticalBranchName());
+    }
+
+    @Test
+    void getPreventiveBranchStatus() {
+        Mockito.when(state.getInstant()).thenReturn(Instant.PREVENTIVE);
+        assertEquals("P", limitingBranchResult.getBranchStatus());
+    }
+
+    @Test
+    void getOutageBranchStatus() {
+        Mockito.when(state.getInstant()).thenReturn(Instant.OUTAGE);
+        assertEquals("O", limitingBranchResult.getBranchStatus());
+    }
+
+    @Test
+    void getCurativeBranchStatus() {
+        Mockito.when(state.getInstant()).thenReturn(Instant.CURATIVE);
+        assertEquals("C", limitingBranchResult.getBranchStatus());
+    }
+
+    @Test
+    void notSupportedStateInstant() {
+        Mockito.when(state.getInstant()).thenReturn(Instant.AUTO);
+        try {
+            limitingBranchResult.getBranchStatus();
+        } catch (CoreValidInvalidDataException e) {
+            assertEquals("Invalid value in CBCORA file, for cnec criticalBranchId", e.getMessage());
+        }
+    }
 }
