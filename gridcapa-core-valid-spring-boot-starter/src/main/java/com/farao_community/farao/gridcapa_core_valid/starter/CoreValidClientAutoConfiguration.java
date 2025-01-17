1e9a375e57f1153a61c9c2523d7f6918fbbd560f
danielthirion <152513244+danielthirion@users.noreply.github.com>
2025-01-17 10:52:43 +0100

    Fix context with correctly scanning CoreValidClient bean (#208)
    * Fix context with correctly scanning CoreValidClient bean
    
    Signed-off-by: thiriondan <daniel.thirion@rte-france.com>
    
    * Add comment
    
    Signed-off-by: thiriondan <daniel.thirion@rte-france.com>
    
    * Fix comment and rename class
    
    Signed-off-by: thiriondan <daniel.thirion@rte-france.com>
    
    * Add empty line
    
    Signed-off-by: thiriondan <daniel.thirion@rte-france.com>
    
    ---------
    
    Signed-off-by: thiriondan <daniel.thirion@rte-france.com>

---
+++ gridcapa-core-valid-spring-boot-starter/src/main/java/com/farao_community/farao/gridcapa_core_valid/starter/CoreValidClientAutoConfiguration.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.farao_community.farao.gridcapa_core_valid.starter;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Ameni Walha {@literal <ameni.walha at rte-france.com>}
+ * This config class allows the scanning of the package by Spring Boot, hence declaring CoreValidClient as a bean
+ */
+@Configuration
+@EnableConfigurationProperties(CoreValidClientProperties.class)
+@ComponentScan
+public class CoreValidClientAutoConfiguration {
+}
